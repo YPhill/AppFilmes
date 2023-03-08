984221265dc88e644b4a1dc6a67ed1ea847634c3
yphill <phelipequeiroz2004@gmail.com>
2023-03-07 22:06:34 -0300

    primeiro

---
+++ app/src/main/java/br/etecia/appfilmes/Filmes.java
@@ -0,0 +1,12 @@
+package br.etecia.appfilmes;
+
+public class Filmes {
+    private String titulo;
+    private int miniatura;
+
+    public Filmes(String titulo, int miniatura) {
+        this.miniatura = miniatura;
+        this.titulo = titulo;
+
+    }
+}
---
+++ app/src/main/java/br/etecia/appfilmes/MyAdapter.java
@@ -0,0 +1,48 @@
+package br.etecia.appfilmes;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context mContext;
+    private List<Filmes> mData;
+
+    public MyAdapter(Context mContext, List<Filmes> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public RecyclerView.ViewHolder extends MyAdapter{
+
+    }
+
+
+
+
+
+
+
+}
+}
